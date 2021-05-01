5e29c2ebb19d8951825ed9af517a163d6412c40b
Charlie Stokes <chstokes7@gmail.com>
2021-05-01 17:38:18 -0500

    add like button adds likes!

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,13 +1,13 @@
 import GalleryItem from '../GalleryItem/GalleryItem';
 import './GalleryList.css'
 
-function GalleryList({pictureArray}) {
+function GalleryList({pictureArray, getPictures}) {
     return(
         <>
         <div className='parentCard'>
 
             {pictureArray.map(picture => (
-                <GalleryItem key={picture.id} picture={picture}/>
+                <GalleryItem key={picture.id} picture={picture} getPictures={getPictures}/>
             ))}
         </div>
         </>
